3892c570ecb1ecc2272cc44c3873bfb674cf4f52
Illusory Stella <XyZ3r7@outlook.com>
2025-10-07 17:04:26 -0400

    Fixed the error of exception

--- src/LiJiangDriver.java
+++
@@ -1,7 +0,0 @@
-public class LiJiangDriver {
-    public static void main(String[] args) {
-        String password = "abcdefghijk1234987896876!@#!%@!#";
-        LiJiangPassword tester = new LiJiangPassword(password);
-        System.out.println(tester.getStrength());
-    }
-}
