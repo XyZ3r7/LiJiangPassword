f198ae81809b3f181c7a1dba707f3e59b4f4f1af
Illusory Stella <XyZ3r7@outlook.com>
2025-10-10 19:43:33 -0400

    Final version

--- src/LiJiangPasswordInterface.java
+++ src/LiJiangPasswordInterface.java
@@ -1,16 +1,16 @@
 public interface LiJiangPasswordInterface {
     // Yang Li, Linpei Jiang
 
-    public final int MINIMUM_LENGTH = 8;
-    public final int MAXIMUM_LENGTH = 20;
-    public final int ASCII_MAX_VALUE = 255;
+    int MINIMUM_LENGTH = 8;
+    int MAXIMUM_LENGTH = 20;
+    int ASCII_MAX_VALUE = 255;
 
-    public int determineStrength() throws PasswordFormatException;
-    public int length();
-    public int differentConsecutiveTypes();
-    public int upperAndLowerCase();
-    public int consecutiveOrders();
-    public int extraOccurrence();
-    public int consecutiveSameChar();
+    int determineStrength() throws PasswordFormatException;
+    int length();
+    int differentConsecutiveTypes();
+    int upperAndLowerCase();
+    int consecutiveOrders();
+    int extraOccurrence();
+    int consecutiveSameChar();
 
 }
